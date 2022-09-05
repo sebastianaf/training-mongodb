c00979bb2bb405659106aec7928660d892096035
sebastianaf <sebastianafanador95@gmail.com>
2022-09-05 12:22:30 -0500

    adding babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-const mongoose = require("mongoose");
+import { MongoClient } from "mongodb";
+import { faker } from "@faker-js/faker";
+
+require("dotenv").config();
+const url = process.env.DB_MONGO_URL;
+
+(async () => {
+  try {
+    const client = new MongoClient(url)
+    const db = client.db("mydb")
+    const customers = db.collection("customers");
+    
+
+    console.log(`Inserting 1000 customers...`);
+    for (let index = 0; index < 1000; index++) {
+      await customers.insertOne({
+        name: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+        address: faker.address.streetAddress(),
+      });
+    }
+    console.log(`Inserting 1000 customers... OK`);
+
+    client.close();
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+/* const mongoose = require("mongoose");
 
 require("dotenv").config();
 const url = process.env.DB_MONGO_URL;
@@ -11,3 +40,4 @@ const Cat = mongoose.model("Cat", { name: String });
 
 const kitty = new Cat({ name: "Zildjian" });
 kitty.save().then(() => console.log("meow"));
+ */
